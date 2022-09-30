7f25cc57b624d687395d487ef59327e22e2e14eb
Teudis Naranjo <teudis2008@gmail.com>
2022-09-30 06:05:38 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+#from routes.intinerary import intineraty_router
+import uvicorn
+
+
+app = FastAPI()
+# Register routes
+#app.include_router(intineraty_router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
