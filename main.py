b098963e5e0b574338f0a018421b26bf71f48bfe
Teudis Naranjo <teudis2008@gmail.com>
2022-10-01 17:24:48 -0400

    Added SQLmodel and updated main.py

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+from database.connection import conn
+from fastapi.responses import RedirectResponse
+
+
 from routes.drone import drone_router
+
 import uvicorn
 
 
 app = FastAPI()
 # Register routes
-app.include_router(drone_router)
+app.include_router(drone_router, prefix="/drone")
+
+@app.on_event("startup")
+def on_startup():
+    conn()
+
+@app.get("/")
+async def home():
+ return RedirectResponse(url="/drone/")
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
--- models/drone.py
+++ models/drone.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel, Field
+from sqlmodel import JSON, SQLModel, Field, Column
 from enum import Enum
-from typing import Optional
+from typing import Optional, List
 
 class Model(str, Enum):
     Lightweight = "Lightweight"
@@ -18,16 +18,28 @@ class State(Enum):
     RETURNING = "RETURNING"
 
 
-class Medication (BaseModel):
+class Medication (SQLModel, table=True):
+    """Class Model Medication"""
+    id: int = Field(default=None, primary_key=True)
     name : str
     weight : float
     code : str
     image :str
 
-class Drone(BaseModel):
+class MedicationUpate (SQLModel):
+    """Class Update Model Medication"""   
+    name : str
+    weight : float
+    code : str
+    image :str
+
+class Drone(SQLModel, table=True):
+    """Class Model Drone"""
+    id: int = Field(default=None, primary_key=True)
     serial_number : str = Field(..., max_length=100)
     model: Model = Field(...)
     weigth_limit: float = Field(..., gt=0, le=500)
     battery_capacity: int = Field(..., gt=0, le=100, )
     state : State = Field(...)
-    medications : Optional[list[Medication]]
+    medications : Optional[List[str]] = Field(sa_column=Column(JSON))
+    
